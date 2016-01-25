31ff6c7dda223def832c0b127a062aecc022a14b
APushkar <APushkar@luxoft.com>
2016-01-25 17:34:15 +0200

    Added timer setup

--- timerUI.py
+++ timerUI.py
@@ -24,9 +24,25 @@ class Example(QMainWindow):
         
         self.systemTrayIcon.setContextMenu(self.trayMenu)
         self.systemTrayIcon.activated.connect(self.on_systemTrayIcon_activated)
-        self.systemTrayIcon.show()         
+        self.systemTrayIcon.show()
         
         self.initUI()
+        
+        self.timeout = 0
+        self.info_timeout = 0;
+       
+        self.ui.lineEdit.textChanged.connect(self.on_text_edit_changed)
+        self.ui.lineEdit.setValidator(QRegExpValidator(QtCore.QRegExp("[0-9]+")))
+        
+        self.ui.pushButton.clicked.connect(self.on_start_timer)
+        
+        self.timer = QtCore.QTimer(self)
+        
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.on_timeout)
+        
+        self.infoTimer = QtCore.QTimer(self)
+        self.infoTimer.timeout.connect(self.on_info_timeout)
         
     @QtCore.pyqtSlot(QSystemTrayIcon.ActivationReason)
     def on_systemTrayIcon_activated(self, reason):
@@ -34,13 +50,50 @@ class Example(QMainWindow):
             self.showAction.setText("Hide")
             self.showNormal()
             self.isShown = True
+    
+    def on_text_edit_changed(self, text):
+        #print ("Input %s" % text)
+        if len(text) == 0: self.timeout = 0
+        else: self.timeout = int(text)
+        self.ui.lcdNumber.display(self.timeout * 60) #display timeout in seconds
+        
+    def on_start_timer(self, triggered):
+        #print ("On start timer")
+        self.timer.start(self.timeout * 60 * 1000)
+        self.info_timeout = self.timeout * 60
+        self.infoTimer.start(1000) # 1 sec duration
+        
+    def on_timeout(self):
+        self.timer.stop()
+        self.infoTimer.stop()
+        self.ui.lcdNumber.display(self.timeout)
+        self.info_timeout = self.timeout * 60
+        self.isShown = True
+        self.showNormal()
+        #print("!On timer!") 
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle("== Notification ==")
+        msgBox.setText("On timer!")
+        msgBox.exec_()
+        
+    def on_info_timeout(self):
+        self.info_timeout -= 1
+        self.ui.lcdNumber.display(self.info_timeout)
         
     def changeEvent(self, event):
         if event.type() == QtCore.QEvent.WindowStateChange:
-            if self.windowState() and QtCore.Qt.WindowMinimized:
+            if self.windowState() & QtCore.Qt.WindowMinimized:                
+                event.accept()
+                self.hide()                
                 event.ignore()
-                self.hide()
-                return 
+            #if self.windowState() and QtCore.Qt.WindowMinimized:
+                #print("minimize")
+                
+                #item.setText("Show")
+                #self.showAction.setText("Show")                
+                #self.hide()
+                #event.ignore()
+                #event.ignore()                
         
     def initUI(self):  
         
